c2c4c05c130f7fe989e9da718853436c5716839a
psantiago <Peter.J.Santiago@gmail.com>
2014-06-27 13:43:50 -0400

    broke all of tim's changes

---
+++ DataModel/CurrentClientSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splity.DataModel
+{
+    public static class CurrentClientSingleton
+    {
+        public static readonly HttpClient Client = new HttpClient();
+
+        static CurrentClientSingleton()
+        {
+            Client.BaseAddress = new Uri("https://thematrixrevolutions.azurewebsites.net/");
+        }
+    }
+}
--- DataModel/ProjectSource.cs
+++ DataModel/ProjectSource.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Splity.DataModel;
 using Splity.Domain;
 
 namespace Splity.Data
@@ -13,24 +14,21 @@ namespace Splity.Data
     /// </summary>
     public sealed class ProjectSource
     {
-        private static readonly HttpClient Client = new HttpClient();
-
-        static ProjectSource()
-        {
-            Client.BaseAddress = new Uri("https://thematrixrevolutions.azurewebsites.net/");
-        }
 
         public static async Task<IEnumerable<Project>> GetProjectsAsync()
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, "api/tickets");
-            HttpResponseMessage response = await Client.SendAsync(request);
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/projects");
+            HttpResponseMessage response = await CurrentClientSingleton.Client.SendAsync(request);
             string jsonString = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<Project[]>(jsonString);
         }
 
         public static async Task<Project> GetProjectsAsync(int id)
         {
-            return await Task.Factory.StartNew(() => FakeData.GetSomeProjects().FirstOrDefault(i => i.Id == id));
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/projects/" + id);
+            var response = await CurrentClientSingleton.Client.SendAsync(request);
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Project>(jsonString);
         }
     }
 }
