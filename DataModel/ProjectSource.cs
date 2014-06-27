f1447f23f5d21e69f1c96e6e89f4d1020c26f19f
TeeBirdTim <tamashburn@gmail.com>
2014-06-27 13:38:50 -0400

    Cause Peter problems.

--- DataModel/ProjectSource.cs
+++ DataModel/ProjectSource.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Splity.Domain;
 
 namespace Splity.Data
@@ -22,7 +23,7 @@ namespace Splity.Data
 
         public static async Task<IEnumerable<Project>> GetProjectsAsync()
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, "api/tickets");
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/project");
             HttpResponseMessage response = await Client.SendAsync(request);
             string jsonString = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<Project[]>(jsonString);
@@ -30,7 +31,23 @@ namespace Splity.Data
 
         public static async Task<Project> GetProjectsAsync(int id)
         {
-            return await Task.Factory.StartNew(() => FakeData.GetSomeProjects().FirstOrDefault(i => i.Id == id));
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/project/"+id);
+            HttpResponseMessage response = await Client.SendAsync(request);
+            string jsonString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Project>(jsonString);
+        }
+
+        public static async Task AddProjectAsync(Project p)
+        {
+            var jsonString = JsonConvert.SerializeObject(p);
+            var request = new HttpRequestMessage(HttpMethod.Post, "api/project");
+            await Client.SendAsync(request);
+        }
+
+        public static async Task DeleteProject(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, "api/project/" + id);
+            await Client.SendAsync(request);
         }
     }
 }
